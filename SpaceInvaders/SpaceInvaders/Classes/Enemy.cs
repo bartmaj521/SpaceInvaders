d6836654d988dfc9f58ed8905144b0ed940570bb
Bartosz Majewski <bartmaj@windowslive.com>
2017-03-02 00:44:27 +0100

    Dodano pierwsze klasy.

---
+++ SpaceInvaders/SpaceInvaders/Classes/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders.Classes
+{
+    class Enemy: GameObject
+    {
+        public override void update(float deltaTime)
+        {
+            
+        }
+    }
+}
--- SpaceInvaders/SpaceInvaders/Classes/GameObject.cs
+++ SpaceInvaders/SpaceInvaders/Classes/GameObject.cs
@@ -4,9 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
 namespace SpaceInvaders.Classes
 {
-    class GameObject
+    abstract class GameObject
     {
+        protected Sprite animationSprite { get; set; }
+        protected int nbAnimationFrames;
+        protected int currentFrame;
+        protected float frameTime;
+        protected float currFrameTime;
+
+        protected FloatRect collider;
+
+        abstract public void update(float deltaTime);
+
+        public GameObject()
+        {
+
+        }
+
+        public GameObject(ref Texture txt, int _nbAnimationFrames, float _frameTime, Vector2f startingPosition, Vector2f Scale)
+        {
+            nbAnimationFrames = _nbAnimationFrames;
+
+            animationSprite.Texture = txt;
+            animationSprite.Position = startingPosition;
+            animationSprite.Scale = Scale;
+            animationSprite.TextureRect = new IntRect(0, 0, (int)animationSprite.Texture.Size.X, (int)animationSprite.Texture.Size.Y / nbAnimationFrames);
+
+            collider.Left = startingPosition.X;
+            collider.Top = startingPosition.Y;    
+            collider.Width = animationSprite.Texture.Size.X * Scale.X;
+            collider.Height = animationSprite.Texture.Size.Y * Scale.Y;
+
+            frameTime = _frameTime;
+        }
+
+
+        
+
+
+
+
+
+
+
     }
 }
