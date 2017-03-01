bd231173814d27291e7a1af16c887c457950eb34
Bartosz Majewski <bartmaj@windowslive.com>
2017-03-01 19:37:56 +0100

    Rozpoczęto projekt.

---
+++ SpaceInvaders/SpaceInvaders/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using SFML.Graphics;
+using SFML.Window;
+
+
+namespace SpaceInvaders
+{
+    static class Program
+    {
+        static void Main()
+        {
+            RenderWindow lol = new RenderWindow(new VideoMode(300, 600), "lol");
+            while (true)
+            {
+
+            }
+        }
+    }
+}
