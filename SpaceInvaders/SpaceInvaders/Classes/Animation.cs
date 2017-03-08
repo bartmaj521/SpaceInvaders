815d8fd9e77a328a1d43d4f0b3a2f8b9ed376bdf
bartmaj521 <bartmaj@windowslive.com>
2017-03-08 13:12:43 +0100

    Aktualizacja GameObject, Player i utworzenie Animation.

---
+++ SpaceInvaders/SpaceInvaders/Classes/Animation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
+namespace SpaceInvaders.Classes
+{
+    class Animation
+    {
+    
+        public Sprite animationSprite { get; set; }
+        protected int[] frames;
+        protected int currentFrame;
+        protected float frameTime;
+        protected float currFrameTime;
+
+        public Animation(ref Texture txt, int[] _frames, float _frameTime, Vector2f startingPosition, Vector2f Scale)
+        {
+            animationSprite = new Sprite();
+            animationSprite.Texture = txt;
+            frames = _frames;
+            frameTime = _frameTime;
+            animationSprite.Position = startingPosition;
+            animationSprite.Scale = Scale;
+            currFrameTime = 0;
+            currentFrame = 0;
+
+        }
+        public void updateAnimation(float deltaTime, Vector2f newPos)
+        {
+            animationSprite.Position = newPos;
+            currFrameTime += deltaTime;
+            if(currFrameTime > frameTime)
+            {
+                currFrameTime = 0;
+                currentFrame++;
+                if(currentFrame >= frames.Length)
+                {
+                    currentFrame = 0;
+                }
+                animationSprite.TextureRect = new IntRect(0, Convert.ToInt32(animationSprite.Texture.Size.Y / frames.Length * frames[currentFrame]), Convert.ToInt32(animationSprite.Texture.Size.X), Convert.ToInt32(animationSprite.Texture.Size.Y / frames.Length * frames[currentFrame] + animationSprite.Texture.Size.Y / frames.Length));
+            }
+        }
+
+    }
+}
--- SpaceInvaders/SpaceInvaders/Classes/Player.cs
+++ SpaceInvaders/SpaceInvaders/Classes/Player.cs
@@ -4,13 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SFML.Window;
+using SFML.Graphics;
+using SFML.System;
+
 namespace SpaceInvaders.Classes
 {
     class Player : GameObject
     {
+        public static float rightBoundary { get; set; }
+        public static float leftBoundary { get; set; }
+
+        public float playerSpeed { get; set; }
+
         public override void update(float deltaTime)
         {
-
+            Vector2f offset = new Vector2f(0,0);
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Left))
+            {
+                offset.X = -playerSpeed * deltaTime;
+            }
+            else if(Keyboard.IsKeyPressed(Keyboard.Key.Right))
+            {
+                offset.X = playerSpeed * deltaTime;
+            }
+            collider.Left += offset.X;
+            if(collider.Left + animation.animationSprite.Texture.Size.X * animation.animationSprite.Scale.X > rightBoundary)
+            {
+                collider.Left = rightBoundary - animation.animationSprite.Texture.Size.X * animation.animationSprite.Scale.X;
+            }
+            if(collider.Left < 0)
+            {
+                collider.Left = 0;
+            }
+            animation.updateAnimation(deltaTime, new Vector2f(collider.Left, collider.Top));
         }
     }
 }
